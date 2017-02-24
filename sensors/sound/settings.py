5713c7657726e446523c591388bca6771b436859
javerdejo <javier_arellano_verdejo@hotmail.com>
2017-02-24 10:39:02 +0100

    Sensors v1 added

---
+++ sensors/sound/settings.py
@@ -0,0 +1,5 @@
+VERBOSE = 0
+INTERVAL = 0.9
+LOGPATH = "/soundmeter"
+FLOGGING = "/var/log/soundmeter.log"
+DATAHTML = "/var/log/soundmeter_data.log"
---
+++ sensors/sound/sm.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# Auth: Javier Arellano-Verdejo (J@Vo)
+# Date: Jul/2016
+
+import usb.core
+import usb.util
+import signal
+import time
+import sys
+
+from settings import (VERBOSE, INTERVAL, LOGPATH, FLOGGING, DATAHTML)
+from global_settings import (STATION_ID)
+
+class SoundMeterGM1356(object):
+
+	class StopException(Exception):
+		pass
+
+	def __init__(self):
+		global _soundsensor
+		_soundsensor = self
+
+		self.fLogging = open(FLOGGING,'a')
+		self.datahtml = open(DATAHTML,'w')
+
+		self._running = False
+
+		self.VENDOR_ID = 0x64bd
+		self.PRODUCT_ID = 0x74e3
+
+		devFound = False
+		while not devFound:
+			self.device = usb.core.find(idVendor=self.VENDOR_ID, idProduct=self.PRODUCT_ID)
+			if self.device is None:
+				self.printlog("Device not found ...")
+				time.sleep(3)
+			else:
+				self.printlog("Device found")
+				devFound = True
+
+		strTime = time.strftime("%H:%M:%S")
+		strDate = time.strftime("%Y-%m-%d")
+
+		self.FILELOG = LOGPATH + "/registersound_" + strDate + "_" + strTime + ".log"
+		self.fileLog = open(self.FILELOG,'w')
+		self.fileLog.write("Sensor ID, Date Time,Measure,Max,Speed,Curve,Range\n")
+
+		if INTERVAL < 0.5:
+			self.PAUSE = 1
+		else:
+			self.PAUSE = INTERVAL
+
+	def start(self):
+		self._running = True
+		peak = 0
+
+		if self.device.is_kernel_driver_active(0):
+			try:
+				self.device.detach_kernel_driver(0)
+				self.printlog("kernel driver detached")
+			except usb.core.USBError as e:
+				self.printlog("Could not detach kernel driver: %s" % str(e))
+				sys.exit("Could not detach kernel driver: %s" % str(e))
+
+		try:
+			self.printlog("Taking measurements of sound ...")
+			while self._running:
+				try:
+					data = "b3 00 00 00 00 00 00 00"
+					buf = data.replace(" ", "").decode('hex')
+					self.device.write(0x02, buf, 1000)
+					ret = self.device.read(0x81, 8, 1000)
+					values = bytearray(ret)
+				except usb.core.USBError:
+					continue
+
+				if len(values) == 8:
+					measure = (256*values[0] + values[1]) / 10.0
+
+					if measure > 130 or measure < 0:
+						measure = 0
+
+					strTime = time.strftime("%H:%M:%S")
+					strDate = time.strftime("%Y-%m-%d")
+
+					if measure > peak:
+						peak = measure
+
+					if (values[2] >> 6) == 1:
+						strSpeed = "Fast"
+					else:
+						strSpeed = "Slow"
+
+					if (values[2] >> 4 & 0x01) == 0:
+						strCurve = "A"
+					else:
+						strCurve = "C"
+
+					range_cfg = values[2] & 0x07
+
+					strDataLog = "%s,%s %s,%2.1f,%2.1f,%s,%s,%d\n" % (STATION_ID,strDate, strTime, measure, peak, strSpeed, strCurve, range_cfg)
+					self.fileLog.write(strDataLog)
+					self.fileLog.flush()
+
+					strDataHtml = "%2.1f %2.1f\n" % (measure, peak)
+					self.datahtml.seek(0)
+					self.datahtml.write(strDataHtml)
+					self.datahtml.flush()
+
+					if VERBOSE == 1:
+						self.printlog("%s %s %s %2.1f db Max: %2.1f db %s %s %d" % (strDate, strTime, STATION_ID, measure, peak, strSpeed, strCurve, range_cfg))
+
+					time.sleep(self.PAUSE)
+			self.stop()
+		except self.__class__.StopException:
+			self.stop()
+
+	def stop(self):
+		self.printlog('Stopping')
+		self.fileLog.close()
+		self.fLogging.close()
+		self.datahtml.close()
+
+	def running(self):
+		self._running = False
+
+	def printlog(self, msg):
+		strTime = time.strftime("%H:%M:%S")
+		strDate = time.strftime("%Y-%m-%d")
+
+		self.fLogging.write("INFO " + strDate + " " + strTime + " => " + msg + "\n")
+		self.fLogging.flush()
+
+# -- Fin de la clase SoundMeterGM1356 ------------------------------
+
+def sigint_handler(signum, frame):
+	_soundsensor.running()
+
+if __name__ == '__main__':
+	signal.signal(signal.SIGINT, sigint_handler)
+	signal.signal(signal.SIGTERM, sigint_handler)
+
+	sm = SoundMeterGM1356()
+	sm.start()
